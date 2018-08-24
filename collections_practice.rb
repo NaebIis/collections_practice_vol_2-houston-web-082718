2e016ac192395fcfe0d01a923884592d2f7227ba
John Grubbs <johnmlowery@gmail.com>
2018-08-24 16:42:51 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,20 @@
-# your code goes here
+
+ def begins_with_r(array)
+  array.all? {|word| word.start_with?("r")}
+ end
+ 
+ def contain_a(array)
+ 	array.find_all {|word| word.include?("a")}
+end
+
+ def first_wa(array)
+  array.find {|prefix| prefix.include?("wa")}
+end
+
+def remove_non_strings(array)
+ 	array.delete_if {|string| string.class != String}
+end
+
+def count_elements(array)
+  array.count { |name| name > 0}
+end
